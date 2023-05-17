d34f6e9fa5a8a09138cef7c3c89dfab9ff7e0b15
Subhransu Panda <subhransupanda2000@gmail.com>
2023-05-17 18:51:45 +0530

    First Commit

---
+++ class firstclass{.java
@@ -0,0 +1,12 @@
+class firstclass {
+    public static void main (String [] args ) {
+        System.out.println ("hello world with java /n");
+        system.out.println ("hello world");
+
+
+    }
+
+}
+
+
+    
